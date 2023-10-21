549fb3c5f71f88e096f340d6e706561793ab92c7
Leena chatterjee <82234003+leenachatterjee@users.noreply.github.com>
2023-10-21 23:18:00 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,17 @@
-hyabk   qnzskmnsa,.m.as,m/,
+class Squares(object):
+    def __init__(self, start, stop):
+       self.start = start
+       self.stop = stop
+
+    def __iter__(self): 
+        return self
+
+    def __next__(self): # next in Python 2
+       if self.start >= self.stop:
+           raise StopIteration
+       current = self.start * self.start
+       self.start += 1
+       return current
+
+
+iterator = Squares(7, 8)
